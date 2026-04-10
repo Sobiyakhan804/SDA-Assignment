2248d8304e821308302feb9d786bde5d36fee759
Sobiyakhan804 <sobiyakhanyousafxaii@gmail.com>
2026-04-10 23:42:35 +0500

    Initial Commit: Original messy code

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,3 @@
-/*
-
-Authors: Akide Liu ; Andrew Wang ; Chi Wang
-Date : 24/9/20
-
-OOP-Project
-Copyright (C) <2020>  Akide Liu ; Andrew Wang ; Chi Wang
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-Description :
-
 */
 #include <iostream>
 #include <string>
@@ -634,9 +611,3 @@ int studentMenu(int ID) {
     return 0;
 
 }
-
-
-
-
-
-
